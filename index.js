ec1d8a2c2bd47da70acbf592b4edcaa315245cc6
root <2105wolf@proton.me>
2024-02-15 01:32:36 +0000

    Selfbot working

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
 console.log("Disboard Autobumper 2024 - Brought to you by Max Fox at https://github.com/pedro-santos21")
 
+require('dotenv').config();
+console.log(process.env);
 
+// discord.js-selfbot setup
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client();
 
-var token
+var token = process.env.TOKEN;
 
-function Bump(channel_id, bump_message):
-	
+function Bump(channel_id, bump_message) {
+	console.log("Bumping...")
+}
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is ready!`);
+})
+
+client.login(token);
